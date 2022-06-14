28ca36be3994fad7c8962750bf0af2820b353be0
Eric W. M <ericmwangi0916@gmail.com>
2022-06-14 14:38:29 +0300

    Started developing a Plugin

---
+++ my-cool-beautiful-plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+    Plugin Name: My Cool Beautiful Plugin
+    Description: The most beautiful plugin being developed.
+    Version: 1.2.0
+    Author: Eric M.
+    Author URI: https://www.datiospecio.com/
+*/
