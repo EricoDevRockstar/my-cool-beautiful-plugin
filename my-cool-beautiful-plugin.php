64095243a534778d09f10902fd8f7da1605d49c8
Eric W. M <ericmwangi0916@gmail.com>
2022-06-15 04:59:53 +0300

    Made modification to our plugin.

--- my-cool-beautiful-plugin.php
+++ my-cool-beautiful-plugin.php
@@ -8,11 +8,38 @@
     Author URI: https://www.datiospecio.com/
 */
 
-function addContentAtEndOfPost($content) {
-    if (is_single() && is_main_query()) {
-        return $content . '<p>My name is Mrefu mwerevu.</p>';
+class WordCountAndTimePlugin {
+   function __construct() {
+    add_action('admin_menu', array($this, 'adminPage'));
+    add_action('admin_init', array($this, 'settings'));
     }
-    return $content;
+
+    function settings() {
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+    }
+
+    function locationHTML() { ?>
+        Hello
+   <?php }
+
+    function adminPage() {
+        add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+    }
+    
+    function ourHTML() { ?>
+        <div class="wrap">
+            <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                    do_settings_sections('word-count-settings-page');
+                    submit_button();
+                ?>
+            </form>
+        </div>
+    <?php }
 }
 
-add_filter('the_content', 'addContentAtEndOfPost');
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
+
